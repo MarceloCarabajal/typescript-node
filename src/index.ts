7d52d5651a14391e6cbc2adc6ba6315e6b907582
MarceloCarabajal <marce.tsgia@gmail.com>
2024-07-26 16:03:50 -0300

    Modulos y Webpack

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./classes/Hero";
+// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
+// import * as HeroClass from "./classes/Hero";
+
+const ironman = new Hero( 'Ironman', 1, 55 );
+
+console.log( ironman );
+console.log( ironman.power );
+
+
+
+
+
